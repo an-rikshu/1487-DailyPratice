5f366a36c4ea886051927707e32567f7d9dd41d9
Antriksh Sharma <antrikshsharma18102@gmail.com>
2022-08-22 11:21:35 +0530

    errorhandling

---
+++ 8. 4Aug2022/primitivedatatype.js
@@ -0,0 +1,8 @@
+let a = null;
+let b = 345;
+let c = true;
+let d = BignInt("567") + BigInt("9")
+let e = "antriksh"
+let f = Symbol("i am a nice symbol")
+let g = undefined
+console.log(a,b,c,d,e,f,g)
